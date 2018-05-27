aeddd0e5fd19034d803094de1eca740ff90c65dc
René Ziegler <ziegler.rene87@gmx.de>
2018-05-27 16:54:53 +0200

    Added more map parsing logik, we now have different tiles

---
+++ Assets/Scripts/Mapping/TileType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Mapping
+{
+    public enum TileType
+    {
+        Void,
+        Path,
+        Buildslot
+    }
+}
