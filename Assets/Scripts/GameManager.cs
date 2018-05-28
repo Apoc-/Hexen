343375ee8ab188bd7e94e2b310a8f819cfe1713a
René Ziegler <ziegler.rene87@gmx.de>
2018-05-29 00:00:57 +0200

    We now have a main Menu Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,67 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Hexen;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts
 {
     class GameManager : Singleton<GameManager>
     {
         public Player Player;
-        public TowerBuildManager TowerBuildManager;
 
-        public void Start()
+        private TowerBuildManager towerBuildManager;
+        public TowerBuildManager TowerBuildManager
+        {
+            get
+            {
+                if (towerBuildManager == null)
+                {
+                    towerBuildManager = FindObjectOfType<TowerBuildManager>();
+                }
+
+                return towerBuildManager;
+            }
+        }
+
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private IEnumerator LoadSceneAsynch(string sceneName)
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+
+            InitGame();
+        }
+
+        private void InitGame()
         {
             InitPlayer();
+            InitTowerBuildManager();
+        }
+
+        public void StartGame()
+        {
+            StartCoroutine(LoadSceneAsynch("GameScene"));
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();   
+        }
+
+        private void InitTowerBuildManager()
+        {
             TowerBuildManager.LoadTowers();
         }
 
