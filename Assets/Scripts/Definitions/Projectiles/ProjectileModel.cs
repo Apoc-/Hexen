c2bfb88b547e3ad426ec86032c8d81cdc349e9f2
René Ziegler <ziegler.rene87@gmx.de>
2018-08-27 00:01:46 +0200

    Missed something

---
+++ Assets/Scripts/Definitions/Projectiles/ProjectileModel.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.Systems.ProjectileSystem;
+using UnityEngine;
+
+namespace Assets.Scripts.Definitions.Projectiles
+{
+    public class ProjectileModel : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            GetComponentInParent<Projectile>().Collide(other);
+        }
+    }
+}
